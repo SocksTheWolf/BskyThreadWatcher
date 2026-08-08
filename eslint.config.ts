b8f1177770cc0a7624f0abe72590ea1540680e8a
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-08-07 21:10:37 -0700

    move eslint to optional

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, globalIgnores } from "eslint/config";
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
-  globalIgnores(["src/wrangler.d.ts"]),
+  globalIgnores(["src/types/*.d.ts"]),
   {
     rules: {
       "@typescript-eslint/ban-ts-comment": "off"
