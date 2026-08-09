7f9bd1b28ae22c6a701a78b4eb8aaf4197b9214e
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-08-09 12:02:27 -0700

    update webhook secret name

--- src/services/discord.ts
+++ src/services/discord.ts
@@ -3,7 +3,7 @@ import isEmpty from "just-is-empty";
 import { Webhook } from "minimal-discord-webhook-node";
 
 export const getDiscordWebhook = (env: Env): DiscordWebhook => {
-  return !isEmpty(env.WEBHOOK) ?
-    new Webhook({url: env.WEBHOOK, throwErrors: false, retryOnLimit: true}) : null;
+  return !isEmpty(env.DISCORD_WEBHOOK) ?
+    new Webhook({url: env.DISCORD_WEBHOOK, throwErrors: false, retryOnLimit: true}) : null;
 }
 export type DiscordWebhook = Webhook | null;
