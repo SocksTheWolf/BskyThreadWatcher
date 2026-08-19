5451d251a28623674ce75d3b9d51071c545eff27
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-08-18 19:52:31 -0700

    update ignores for eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,7 +4,7 @@ import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(["src/definitions/*.d.ts"]),
+  globalIgnores(["src/@types/*.d.ts"]),
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
   tseslint.configs.strictTypeChecked,
   {
